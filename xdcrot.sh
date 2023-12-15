973cb3dba3bf15dbce1ed3a5fed48c4e59f8be00
cannotbenormal <113307480+cannotbenormal@users.noreply.github.com>
2023-12-15 12:57:35 +0700

    Update xdcrot.sh

--- xdcrot.sh
+++ xdcrot.sh
@@ -1,5 +1,6 @@
 wget https://github.com/subhra74/xdm/releases/download/8.0.29/xdman_gtk_8.0.29_amd64.deb && 
 dpkg -i xdman_gtk_8.0.29_amd64.deb &&
+sleep 5 &&
 wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz &&
 tar -xf ffmpeg-master-latest-linux64-gpl.tar.xz &&
 sleep 2 &&
