271c88589a9f028994738d1c1583bf20dad0a458
cannotbenormal <113307480+cannotbenormal@users.noreply.github.com>
2023-12-15 12:19:01 +0700

    Create xdcrot.sh

---
+++ xdcrot.sh
@@ -0,0 +1,7 @@
+wget https://github.com/subhra74/xdm/releases/download/8.0.29/xdman_gtk_8.0.29_amd64.deb && 
+dpkg -i xdman_gtk_8.0.29_amd64.deb &&
+wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz &&
+tar -xf ffmpeg-master-latest-linux64-gpl.tar.xz &&
+sleep 2 &&
+cp ./ffmpeg-master-latest-linux64-gpl/bin/{ffmpeg,ffprobe} /opt/xdman/ &&
+rm -rf ffmpeg-master-latest-linux64-gpl.tar.xz ffmpeg-master-latest-linux64-gpl
